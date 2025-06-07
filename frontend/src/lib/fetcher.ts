425af8166f359519409799661b2296004d0bfb18
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-06-07 16:12:17 +0300

    feat: fetcher installed

---
+++ frontend/src/lib/fetcher.ts
@@ -0,0 +1,8 @@
+// lib/fetcher.ts
+export async function fetcher<T>(url: string): Promise<T> {
+  const res = await fetch(url)
+  if (!res.ok) {
+    throw new Error('Network response was not ok')
+  }
+  return res.json()
+}
