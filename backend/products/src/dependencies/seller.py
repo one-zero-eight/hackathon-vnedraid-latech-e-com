9d81452a67e3ab769fc358fc3566537db71968dd
bebra_dev <fivan999@yandex.ru>
2025-06-08 15:03:55 +0300

    fix: get seller id

--- backend/products/src/dependencies/seller.py
+++ backend/products/src/dependencies/seller.py
@@ -11,9 +11,9 @@ class SellerIdProvider(Provider):
     @provide
     async def get_user_id(self, request: Request) -> SellerIdScheme:
         seller_id = request.headers.get("X-User-Id")
-        if seller_id is None:
+        if seller_id and seller_id.isnumeric():
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="no X-User-Id header was provided",
+                detail="invalid X-User-Id",
             )
         return SellerIdScheme(seller_id=int(seller_id))
